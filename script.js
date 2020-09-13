1ab81c5b05512240b7ad784b671150c215b7ca55
Kittipob Wongkanta <59160342@go.buu.ac.th>
2020-09-13 14:18:59 +0700

    init

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
 //     let firstName;
 //     const yearOfBirth = 1990 ;
 //     if(passedTest){
-       
+
 //         firstName = 'John';
 //     }
 //     console.log(firstName+ ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
@@ -114,40 +114,40 @@
 // Arrow functions
 
 //ES5
-var box5 = {
-    color: 'green',
-    postition: 1,
-    clickMe: function(){
-        var self = this;
-        document.querySelector('.green')
-        .addEventListener('click',
-        function() {
-            var str = 'This is box number' + self.postition + 
-            ' and is it '
-            + self.color;
-            alert(str);
-        });
-    }
-}
+// var box5 = {
+//     color: 'green',
+//     postition: 1,
+//     clickMe: function(){
+//         var self = this;
+//         document.querySelector('.green')
+//         .addEventListener('click',
+//         function() {
+//             var str = 'This is box number' + self.postition +
+//             ' and is it '
+//             + self.color;
+//             alert(str);
+//         });
+//     }
+// }
 // box5.clickMe();
 
 //ES6
-const box6 = {
-    color: 'green',
-    postition: 1,
-    clickMe: function(){
+// const box6 = {
+//     color: 'green',
+//     postition: 1,
+//     clickMe: function(){
 
-        document.querySelector('.green')
-        .addEventListener('click', 
-        () => {
-            var str = 'This is box number' + this.postition + 
-            ' and is it '
-            + this.color;
-            alert(str);
-        });
-    }
-}
-box6.clickMe();
+//         document.querySelector('.green')
+//         .addEventListener('click',
+//         () => {
+//             var str = 'This is box number' + this.postition +
+//             ' and is it '
+//             + this.color;
+//             alert(str);
+//         });
+//     }
+// }
+// box6.clickMe();
 
 // const box66 = {
 //     color: 'green',
@@ -155,9 +155,9 @@ box6.clickMe();
 //     clickMe: () => {
 
 //         document.querySelector('.green')
-//         .addEventListener('click', 
+//         .addEventListener('click',
 //         () => {
-//             var str = 'This is box number' + this.postition + 
+//             var str = 'This is box number' + this.postition +
 //             ' and is it '
 //             + this.color;
 //             alert(str);
@@ -166,14 +166,11 @@ box6.clickMe();
 // }
 // box66.clickMe();
 
-
-
-
-function Person(name){
-    this.name = name;
-}
+// function Person(name){
+//     this.name = name;
+// }
 //ES5
-// Person.prototype.myFriends5 = 
+// Person.prototype.myFriends5 =
 // function(friends){
 
 //     var arr = friends.map(function(el){
@@ -182,11 +179,11 @@ function Person(name){
 //     console.log(arr);
 // }
 
-var friends = ['Bob','Jane','Mark'];
+// var friends = ['Bob','Jane','Mark'];
 // new Person('John').myFriends5(friends);
 
 //ES6
-// Person.prototype.myFriends6 = 
+// Person.prototype.myFriends6 =
 // function(friends){
 
 //     var arr = friends.map(el =>
@@ -224,7 +221,7 @@ var friends = ['Bob','Jane','Mark'];
 
 // function calcAgeRetirement(year){
 //     const age = new Date().getFullYear() - year;
-//     return [age, 65 - age]    
+//     return [age, 65 - age]
 // }
 
 // const [age2,retirement] = calcAgeRetirement(1990);
@@ -242,11 +239,9 @@ var friends = ['Bob','Jane','Mark'];
 //     cur.style.backgroundColor = 'dodgerBlue'
 // })
 
-
 //ES6
 // const boxesArr6 = Array.from(boxes);
 // Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
-
 
 //ES5
 // for(var i = 0; i< boxesArr5.length;i++){
@@ -279,7 +274,6 @@ var friends = ['Bob','Jane','Mark'];
 // console.log(ages.findIndex(cur => cur >= 18));
 // console.log(ages.find(cur => cur >= 18));
 
-
 /////////////////////
 /// Spread operator
 
@@ -310,13 +304,12 @@ var friends = ['Bob','Jane','Mark'];
 
 // Array.from(all).forEach(cur => cur.style.color = 'purple')
 
-
 /////////////////////////////////////////////////////////////
 /// Rest parameters
 
 //ES5
 // function isFullAge5(){
-    // console.log(arguments);
+// console.log(arguments);
 //     var argsArr =
 //     Array.prototype.slice.call(arguments);
 
@@ -362,16 +355,15 @@ var friends = ['Bob','Jane','Mark'];
 
 // ES5
 // function SmitePerson(firstName,yearOfBirth,lastName,nationality){
-    
+
 //     lastName === undefined ? lastName = "Smith" : lastName = lastName;
 //     nationality === undefined ? nationality = 'american' : nationality = nationality;
-    
+
 //     this.firstName = firstName;
 //     this.lastName = lastName;
 //     this.yearOfBirth = yearOfBirth;
 //     this.nationality = nationality;
 // }
-
 
 //ES6
 // function SmithPerson(firstName,yearOfBirth,lastName = 'Smith',nationality = 'american'){
@@ -382,7 +374,6 @@ var friends = ['Bob','Jane','Mark'];
 // }
 // var john = new SmithPerson('John',1990);
 // var emily = new SmithPerson('Emily',1983,'Diaz','spanish');
-
 
 ///////////////////////////////////////////////////////////////
 // Maps
@@ -397,16 +388,14 @@ var friends = ['Bob','Jane','Mark'];
 // question.set(true,'Correct answer :D');
 // question.set(false,'Wrong please try again!');
 
-
 // console.log(question.get('question'));
 // console.log(question.size);
 
 // if(question.has(4)){
-    // question.delete(4);
+// question.delete(4);
 //     console.log('Answer 4 is hear')
 // }
 // question.clear();
-
 
 // question.forEach((value,key) => console.log(`This is ${key}, and it's set to ${value}`));
 
@@ -422,40 +411,37 @@ var friends = ['Bob','Jane','Mark'];
 ///////////////////////
 /// classes
 
-//ES5 
-var Person5 = function(name,yearOfBirth,job){
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-}
+//ES5
+// var Person5 = function(name,yearOfBirth,job){
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+// }
 
-Person5.prototype.calculateAge = 
-function(){
-    var age = new Date().getFullYear() - this.yearOfBirth;
-    console.log(age);
-}
+// Person5.prototype.calculateAge =
+// function(){
+//     var age = new Date().getFullYear() - this.yearOfBirth;
+//     console.log(age);
+// }
 
-var Athlete5 = function(name,yearOfBirth,job,olymicGames,medals){
-    Person5.call(this,name,yearOfBirth,job);
-    this.olymicGames = olymicGames;
-    this.medals = medals;
-}
+// var Athlete5 = function(name,yearOfBirth,job,olymicGames,medals){
+//     Person5.call(this,name,yearOfBirth,job);
+//     this.olymicGames = olymicGames;
+//     this.medals = medals;
+// }
 
-Athlete5.prototype = Object.create(Person5.prototype);
+// Athlete5.prototype = Object.create(Person5.prototype);
 
-Athlete5.prototype.wonMedal =
-function(){
-    this.medals++;
-    console.log(this.medals);
-}
+// Athlete5.prototype.wonMedal =
+// function(){
+//     this.medals++;
+//     console.log(this.medals);
+// }
 
-var johnAthlete5 = new Athlete5('John',1990,'swimmer',3,10);
- 
-johnAthlete5.calculateAge();
-johnAthlete5.wonMedal();
+// var johnAthlete5 = new Athlete5('John',1990,'swimmer',3,10);
 
-
-
+// johnAthlete5.calculateAge();
+// johnAthlete5.wonMedal();
 
 // var john = new Person5('John',1998,'teacher');
 
@@ -466,7 +452,7 @@ johnAthlete5.wonMedal();
 //         this.job = job;
 //     }
 //     calculateAge(){
-//         var age = new Date().getFullYear - this.yearOfBirth;
+//         var age = new Date().getFullYear() - this.yearOfBirth;
 //         console.log(age);
 //     }
 //     static greeting(){
@@ -476,3 +462,108 @@ johnAthlete5.wonMedal();
 
 // const john6 = new Person6('John',1990,'teacher');
 // Person6.greeting();
+
+// class Athlete6 extends Person6 {
+//     constructor(name,yearOfBirth,job,olympicGames,medals){
+//         super(name,yearOfBirth,job);
+//         this.olympicGames = olympicGames;
+//         this.medals = medals;
+//     }
+
+//     wonMedal(){
+//         this.medals ++;
+//         console.log(this.medals);
+//     }
+// }
+
+// const johnAthlete6 = new Athlete6('John',1990,'swimer',3,10);
+
+// johnAthlete6.wonMedal();
+// johnAthlete6.calculateAge();
+
+class Element {
+  constructor(name, buildYear) {
+    this.name = name;
+    this.buildYear = buildYear;
+  }
+}
+
+class Park extends Element {
+  constructor(name, buildYear, area, numTress) {
+    super(name, buildYear);
+    this.area = area;
+    this.numTress = numTress;
+  }
+
+  treeDesity() {
+    const density = this.numTress / this.area;
+    console.log(
+      `${this.name} has a tree density of ${density} tress per square km.`
+    );
+  }
+}
+
+class street extends Element {
+  constructor(name, buildYear, length, size = 3) {
+    super(name, buildYear);
+    this.length = length;
+    this.size = size;
+  }
+
+  classifyStreet() {
+    const classification = new Map();
+    classification.set(1, "tiny");
+    classification.set(2, "small");
+    classification.set(3, "normal");
+    classification.set(4, "big");
+    classification.set(5, "huge");
+    console.log(
+      `${this.name}, build in ${this.buildYear}, is a ${classification.get(
+        this.size
+      )} street.`
+    );
+  }
+}
+
+const allPark = [new Park('Green Park',1987,0.2,215), 
+new Park('National Park',1894,2.9,3541),
+new Park('Oak Park',1953,0.4,949)];
+
+const allSreets = [new street('Ocean Avenue',1999,1.1,4),
+new street('Evergreen Steet',2008,2.7,2),
+new street('4th Street',2015,0.8),
+new street('Sunset Boulevard',1982,2.5,5)];
+
+
+function caclc(arr){
+const sum = arr.reduce((prev,cur,index) => prev + cur, 0)
+return [sum , sum / arr.length];
+
+}
+
+function reportParks(p){
+console.log('----------------Parks Report-----------------');
+
+p.forEach(el => el.treeDesity() );
+
+const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+const [tatolAge,avgAge] = caclc(ages);
+console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
+
+const i = p.map(el => el.numTress).findIndex(el => el >= 1000);
+console.log(`${p[i].name} has more than 1000 trees`)
+
+}
+
+function reportStreets(s){
+
+    console.log('----------------Streets Report-----------------');
+
+    const [totalLength , avgLength] = caclc(s.map(el => el.length));
+    console.log(`Our ${s.length} strees have a total length of ${totalLength} km, with an average of ${avgLength} km.`)
+
+    s.forEach(el => el.classifyStreet());
+}
+
+reportParks(allPark);
+reportStreets(allSreets);
